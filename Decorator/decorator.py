4532a935074c08874d7b7c3497ef1cbfaba6fead
Yuhai Li <liyuhai6666@gmail.com>
2017-08-15 21:22:35 -0700

    finish decorator

---
+++ Decorator/decorator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from functools import wraps
+
+HOST_DOCKER = 0
+
+def docker_host_required(f):
+    
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if args[0].type != HOST_DOCKER:
+            raise Exception("Not docker host")
+        else:
+            return f(*args, **kwargs)
+    return wrapper
+
+
+class Host(object):
+    
+    def __init__(self, type):
+        self.type = type
+
+    @docker_host_required
+    def create_container(self):
+        print("create container")
+
+
+if __name__ == '__main__':
+    # initialize Host
+    host = Host(HOST_DOCKER)
+    host.create_container()
+    print("")
+    # reinitialize Host
+    host = Host(1)
+    host.create_container()
